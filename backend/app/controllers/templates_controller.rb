a43084689ab00a144bc79e8020153f5b07862245
Andrew Davisson <akdavisson4@gmail.com>
2019-11-12 09:57:54 -0700

    created seed data and index for templates

--- backend/app/controllers/templates_controller.rb
+++ backend/app/controllers/templates_controller.rb
@@ -1,2 +1,6 @@
 class TemplatesController < ApplicationController
+  def index
+    templates = Template.all
+    render json: templates, only: [:title, :story, :hints]
+  end
 end
--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,51 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Template.create(title: 'Sibling Rivalry', story: [
+    'There once was a young ',
+    ' from ',
+    '. They had ',
+    ' siblings that all share the same ',
+    '. One day, ',
+    ', the oldest sibling decided they had had enough and bought a new ',
+    ' to use for transportation. This new mode of transportation was so ',
+    ' that all of the other siblings felt ',
+    '. They had enough and now they all ',
+    ' to get around.'
+  ], hints: [
+    'animal',
+    'place',
+    'number',
+    'mode of transportation',
+    'name',
+    'animal',
+    'adjective',
+    'feeling',
+    'movement'
+  ]);
+
+Template.create(title: 'Double Life', story: [
+    '',
+    ' was a ',
+    ' by day, but a ',
+    ' by night. Nobody knew of the double life they were leading until they went to ',
+    ' and ran into ',
+    '. After being offered the part of a ',
+    ' in the next major motion picture titled ',
+    ', they declined so that their secret identity would not be revealed. Hollywood was ',
+    ', but heading home to their ',
+    ', ',
+    ', felt great.'
+  ], hints: [
+    'name',
+    'occupation',
+    'occupation',
+    'place',
+    'actor or actress',
+    'occupation',
+    'made up movie name',
+    'feeling',
+    'pet',
+    'name'
+  ])
