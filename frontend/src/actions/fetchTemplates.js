52ded765a8b3846024e0c9521a8a9cb42ab50366
Andrew Davisson <akdavisson4@gmail.com>
2019-11-13 20:13:28 -0700

    Added Foreman to manage rails and react server

--- frontend/src/actions/fetchTemplates.js
+++ frontend/src/actions/fetchTemplates.js
@@ -1,7 +1,7 @@
 export const fetchTemplates = () => {
   return (dispatch) => {
     dispatch({type: 'LOADING_TEMPLATES'})
-    fetch('http://localhost:3000/templates')
+    fetch('http://localhost:3001/templates')
       .then(resp => resp.json())
       .then(templates => {
         dispatch({type: 'ADD_TEMPLATES', templates: templates})
