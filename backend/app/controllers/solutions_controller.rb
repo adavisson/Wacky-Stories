43a953a2847b6b763183ed41d414b4ee2aacc7f3
Andrew Davisson <akdavisson4@gmail.com>
2019-11-15 10:51:04 -0700

    added solutions controller actions

--- backend/app/controllers/solutions_controller.rb
+++ backend/app/controllers/solutions_controller.rb
@@ -1,4 +1,9 @@
 class SolutionsController < ApplicationController
+  def index
+    solutions = Solution.all
+    render json: solutions, only:[:id, :title, :words]
+  end
+
   def show
     solution = Solution.find(params[:id])
     render json: solution, only: [:id, :title, :words]
