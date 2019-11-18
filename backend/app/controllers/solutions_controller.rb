ecca16a828e46f963d20e3082d1a83c3c75fdf96
Andrew Davisson <akdavisson4@gmail.com>
2019-11-18 10:06:33 -0700

    added template_id to render

--- backend/app/controllers/solutions_controller.rb
+++ backend/app/controllers/solutions_controller.rb
@@ -1,11 +1,11 @@
 class SolutionsController < ApplicationController
   def index
     solutions = Solution.all
-    render json: solutions, only:[:id, :title, :words]
+    render json: solutions, only:[:id, :template_id, :title, :words]
   end
 
   def show
     solution = Solution.find(params[:id])
-    render json: solution, only: [:id, :title, :words]
+    render json: solution, only: [:id, :template_id, :title, :words]
   end
 end
