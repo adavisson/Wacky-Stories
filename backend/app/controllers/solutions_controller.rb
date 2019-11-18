0ae3c106d998ff3ef2d9db7a0b246d9db6794499
Andrew Davisson <akdavisson4@gmail.com>
2019-11-18 11:02:37 -0700

    Added create route for solutions

--- backend/app/controllers/solutions_controller.rb
+++ backend/app/controllers/solutions_controller.rb
@@ -8,4 +8,9 @@ class SolutionsController < ApplicationController
     solution = Solution.find(params[:id])
     render json: solution, only: [:id, :template_id, :title, :words]
   end
+
+  def create
+    solution = Solution.find_or_create_by(title: params[:title], words: params[:words], template_id: params[:template_id])
+    render json: solution, only: [:id, :template_id, :title, :words]
+  end
 end
